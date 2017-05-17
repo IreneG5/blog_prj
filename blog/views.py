1834dafa65591b0eba0e5f166369b560ca264ba2
Irene <irene.g5555@gmail.com>
2017-05-17 23:21:18 +0100

    Add a counter and categorise posts using tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,11 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "blogposts.html", {'posts':posts})
+
+
+def post_detail(request, id):
+    post  = get_object_or_404(Post, pk=id)
+    post.views += 1 # clock up the number of post views
+    post.save()
+    return render(request, "postdetail.html",{'post': post})
+
