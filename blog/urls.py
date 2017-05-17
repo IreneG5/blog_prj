9e29c9907446c303e9f92a309053709b6cbfea82
Irene <irene.g5555@gmail.com>
2017-05-18 00:01:33 +0100

    Add menu and most popular posts view and template

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ import views
 
 urlpatterns = [
     url(r'^blog/$', views.post_list),
+    url(r'^blog/popular/$', views.popular_post_list),
     url(r'^blog/(?P<id>\d+)/$', views.post_detail),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "blogposts.html", {'posts':posts})
 
+def popular_post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-views')
+    return render(request, "popularposts.html", {'posts':posts})
+
+
 
 def post_detail(request, id):
     post  = get_object_or_404(Post, pk=id)
