8b7649e2a0a60d72da7f02702b0240f8ab70c209
Irene <irene.g5555@gmail.com>
2017-05-26 01:06:45 +0100

    Add form to create blog posts + images

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^blog/$', views.post_list),
     url(r'^blog/popular/$', views.popular_post_list),
     url(r'^blog/(?P<id>\d+)/$', views.post_detail, name="views-post-detail"),
-
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^blog/(?P<id>\d+)/edit$', views.edit_post),
 
 ]
